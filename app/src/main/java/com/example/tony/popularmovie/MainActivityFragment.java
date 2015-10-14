52ebab4aa526b8e619d8e65a41a1bc520e63f282
tonymeng66 <tonymeng66@hotmail.com>
2015-10-15 01:58:01 +0800

    Try out Gridview using test images

---
+++ app/src/main/java/com/example/tony/popularmovie/MainActivityFragment.java
@@ -0,0 +1,32 @@
+package com.example.tony.popularmovie;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        GridView gridview = (GridView) rootView.findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(getActivity()));
+
+        return rootView;
+    }
+}
+
