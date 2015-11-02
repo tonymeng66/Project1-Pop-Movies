68c89e65e74cacc8cbcc991ee97dadc7d56d6286
tonymeng66 <tonymeng66@hotmail.com>
2015-11-03 02:12:13 +0800

    docs:add copy right statement and class description

--- app/src/main/java/com/example/tony/popularmovie/MainActivityFragment.java
+++ app/src/main/java/com/example/tony/popularmovie/MainActivityFragment.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.tony.popularmovie;
 
 import android.content.Intent;
@@ -30,6 +46,11 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+/**
+ * Populate the main page with an GridView which is filled by movie posters.
+ * Fetch movie information from "TheMovieDB" and parse it in the background , then pass it to the MoviePosterAdapter to update the main page.
+ */
 
     public class MainActivityFragment extends Fragment {
 
